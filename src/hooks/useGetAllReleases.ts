ed062881c4033e0ae12bbbf585ceafeb75ed4315
Chuang Cao(CC) <chcao@redhat.com>
2025-05-21 23:34:54 +0800

    build(KONFLUX-3835): init the release monitor dashboard
    * Add a new page next to Namespaces which is called Release Monitor
    * Check all the valid Release records under Namespaces you can see
    * Add filter status, application, releaseplan and tenant

---
+++ src/hooks/useGetAllReleases.ts
@@ -0,0 +1,59 @@
+import React from 'react';
+import { PipelineRunLabel } from '../consts/pipelinerun';
+import { K8sQueryReleaseListItems } from '../k8s';
+import { ReleaseModel } from '../models';
+
+export interface Release {
+  releasePlan: string;
+  application: string;
+  tenant: string;
+  name: string;
+  completionTime: string;
+  status: string;
+  component: string;
+}
+
+/**
+ * Get all the valid releases across the namespaces that you can see.
+ *
+ * @returns the array of the Release interface.
+ */
+export const useGetAllReleases = (loaded, namespaces) => {
+  const [releases, setData] = React.useState<Release[]>([]);
+
+  React.useEffect(() => {
+    const fetchAll = async (): Promise<Release[]> => {
+      const results: Release[] = [];
+      const namespacePromises = namespaces.map(async (namespace) => {
+        const nsReleases = await K8sQueryReleaseListItems({
+          model: ReleaseModel,
+          queryOptions: { ns: namespace.metadata.name },
+        });
+        nsReleases.forEach((re) => {
+          const applicationName = re.metadata?.labels?.[PipelineRunLabel.APPLICATION] ?? '';
+          const componentName = re.metadata?.labels?.[PipelineRunLabel.COMPONENT] ?? '';
+          const completionTime = re.status?.completionTime ?? '';
+          // Ingore the records that with empty component name inside labels
+          if (componentName) {
+            results.push({
+              releasePlan: re.spec.releasePlan,
+              application: applicationName,
+              tenant: namespace.metadata.name,
+              name: re.metadata.name,
+              completionTime,
+              status: re.status?.conditions[0].reason ?? 'Failed',
+              component: componentName,
+            });
+          }
+        });
+      });
+      await Promise.all(namespacePromises);
+      setData(results);
+      return results;
+    };
+
+    void fetchAll();
+  }, [loaded, namespaces]);
+
+  return releases;
+};
